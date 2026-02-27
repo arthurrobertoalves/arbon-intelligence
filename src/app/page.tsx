d78277d666b873debaddaf493026eaf8147cf49e
Arthur Roberto Alves <arthurrobertoalves@gmail.com>
2026-02-26 23:50:54 -0300

    feat: add navbar with initial structure and base styling

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Navbar from "../components/Header";
+import "./globals.css";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-white">
+      <Navbar />
+      
+    </div>
+  );
+}
